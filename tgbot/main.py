bd5b0dc7788d52ad8b0d2ba7758c6b624ad4869b
dsazanov <astan12324@gmail.com>
2025-04-11 17:12:24 +0600

    add by date filter

--- tgbot/main.py
+++ tgbot/main.py
@@ -23,7 +23,7 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(funcName)s - %(message)s",
     handlers=[
-        logging.FileHandler("bot.log", encoding="utf-8"),
+        # logging.FileHandler("bot.log", encoding="utf-8"),
         logging.StreamHandler(),
     ],
 )
